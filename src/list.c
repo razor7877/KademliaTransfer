e768a4f549a1ac47c2d3a6c4946a152f8177efe0
Hugo Lumet <hugo.lumet@protonmail.com>
2025-10-06 13:37:06 +0200

    Adding add_back primitive

--- src/list.c
+++ src/list.c
@@ -4,19 +4,42 @@ void add_front(struct DList* list, struct Peer* peer) {
   pointer_not_null(list, "Error in add_front the list is unitialized!\n");
   pointer_not_null(peer, "Error in add_front the peer is unitialized!\n");
 
-  struct DNode* new = (struct DNode*)malloc(sizeof(struct DNode));
-  pointer_not_null(new, "Error in add_front the new node is unitialized!\n");
+  struct DNode* new_node = (struct DNode*)malloc(sizeof(struct DNode));
+  pointer_not_null(new_node,
+                   "Error in add_front the new_node node is unitialized!\n");
 
-  new->peer = peer;
-  new->next = list->head;
-  new->prev = NULL;
+  new_node->peer = peer;
+  new_node->next = list->head;
+  new_node->prev = NULL;
 
   if (!list->head) {
-    list->head = list->tail = new;
+    list->head = list->tail = new_node;
     list->size = 1;
   } else {
-    list->head->prev = new;
-    list->head = new;
+    list->head->prev = new_node;
+    list->head = new_node;
+    list->size += 1;
+  }
+}
+
+void add_back(struct DList* list, struct Peer* peer) {
+  pointer_not_null(list, "Error in add_back the list is unitialized!\n");
+  pointer_not_null(peer, "Error in add_back the peer is unitialized!\n");
+
+  struct DNode* new_node = (struct DNode*)malloc(sizeof(struct DNode));
+  pointer_not_null(new_node,
+                   "Error in add_back the new_node node is unitialized!\n");
+
+  new_node->peer = peer;
+  new_node->next = NULL;
+  new_node->prev = list->tail;
+
+  if (!list->head) {
+    list->head = list->tail = new_node;
+    list->size = 1;
+  } else {
+    list->tail->next = new_node;
+    list->tail = new_node;
     list->size += 1;
   }
 }
