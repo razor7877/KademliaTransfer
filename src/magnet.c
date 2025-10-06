28e5b0052680bc82b3b6995c9c7ae02a59882ed7
Hugo Lumet <hugo.lumet@protonmail.com>
2025-10-06 17:20:04 +0200

    Implements the creation magnet uri Scheme from file

--- src/magnet.c
+++ src/magnet.c
@@ -1,17 +1,35 @@
 #include "magnet.h"
 
-struct FileMagnet* create_magnet(char* filename, size_t filename_len, char* contents, size_t contents_len) {
-    return NULL;
+#include <string.h>
+
+struct FileMagnet* create_magnet(char* filename, size_t filename_len,
+                                 char* contents, size_t contents_len) {
+  struct FileMagnet* new_magnet =
+      (struct FileMagnet*)malloc(sizeof(struct FileMagnet));
+  pointer_not_null(new_magnet,
+                   "Error in create_magnet the new_magnet is unitialized!\n");
+
+  new_magnet->display_name = (char*)malloc(sizeof(char) * filename_len);
+  pointer_not_null(new_magnet->display_name,
+                   "Error in create_magnet display_name alloc failed!\n");
+  memcpy(new_magnet->display_name, filename, filename_len);
+  new_magnet->exact_length = contents_len;
+  SHA256(contents, contents_len, new_magnet->file_hash);
+  new_magnet->address_tracker = NULL;
+  new_magnet->web_seed = NULL;
+  new_magnet->acceptable_source = NULL;
+  new_magnet->exact_source = NULL;
+  new_magnet->keyword_topic = NULL;
+  new_magnet->manifest_topic = NULL;
+  new_magnet->select_only = NULL;
+  new_magnet->peer_addresses = NULL;
+  return new_magnet;
 }
 
-char* save_magnet_to_uri(struct FileMagnet* magnet) {
-    return NULL;
-}
+char* save_magnet_to_uri(struct FileMagnet* magnet) { return NULL; }
 
 struct FileMagnet* parse_magnet_from_uri(char* contents, size_t len) {
-    return NULL;
+  return NULL;
 }
 
-void free_magnet(struct FileMagnet* magnet) {
-    free(magnet);
-}
+void free_magnet(struct FileMagnet* magnet) { free(magnet); }
