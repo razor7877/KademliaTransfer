fecedf6befdb33924f3e59f6d3cc8e8e962c0cac
Hugo Lumet <hugo.lumet@protonmail.com>
2025-10-13 18:51:14 +0200

    Implements the base of update_bucket_peers

--- include/bucket.h
+++ include/bucket.h
@@ -1,25 +1,25 @@
 #pragma once
 
-#include "shared.h"
 #include "list.h"
 #include "peer.h"
+#include "shared.h"
 
 /**
  * @file bucket.h
  * @brief Data structures and constants for the Kademlia k-buckets
- * 
- * 
+ *
+ *
  */
 
 /**
  * @brief The number of node entries in a given k-bucket
- * 
+ *
  */
 #define BUCKET_SIZE K_VALUE
 
 /**
  * @brief The number of buckets in the k-buckets structure
- * 
+ *
  */
 #define BUCKET_COUNT 8
 
@@ -27,4 +27,4 @@ typedef struct DList Buckets[BUCKET_COUNT];
 
 struct Peer** find_closest_peers(Buckets buckets, HashID* target, int n);
 
-void update_bucket_peers(struct Peer* peer);
+void update_bucket_peers(Buckets bucket, struct Peer* peer);
--- src/bucket.c
+++ src/bucket.c
@@ -1,91 +1,127 @@
+#include "bucket.h"
+
 #include <openssl/evp.h>
 #include <string.h>
 
-#include "bucket.h"
+#include "list.h"
 #include "log.h"
 #include "shared.h"
-#include "list.h"
 
 /**
  * @brief Gets the bucket index depending on the distance from our node
- * 
+ *
  * @param distance The distance from our node
- * @return int Returns the bucket to choose for this distance, or -1 if the node is ourselves
+ * @return int Returns the bucket to choose for this distance, or -1 if the node
+ * is ourselves
  */
 static int get_bucket_index(HashID* distance) {
-    for (int byte = 0; byte < sizeof(HashID); byte++) {
-        unsigned char byte_value = *distance[byte];
-        for (int bit = 0; bit < 8; bit++) {
-            if (byte_value & (0x80 >> bit)) {
-                log_msg(LOG_WARN, "byte is %d and bit is %d", byte, bit);
-                return byte * 8 + bit;
-            }
-        }
+  for (int byte = 0; byte < sizeof(HashID); byte++) {
+    unsigned char byte_value = *distance[byte];
+    for (int bit = 0; bit < 8; bit++) {
+      if (byte_value & (0x80 >> bit)) {
+        log_msg(LOG_WARN, "byte is %d and bit is %d", byte, bit);
+        return byte * 8 + bit;
+      }
     }
+  }
 
-    return -1;
+  return -1;
 };
 
 struct Peer** find_closest_peers(Buckets buckets, HashID* target, int n) {
-    // log_msg(LOG_DEBUG, "Searching for %d closest peers to ")
-    struct Peer** result = calloc(n, sizeof(struct Peer*));
-    pointer_not_null(result, "find_closest_peers malloc error");
-    
-    char target_str[sizeof(HashID) * 2 + 1] = {0};
-    sha256_to_hex(target, target_str);
+  // log_msg(LOG_DEBUG, "Searching for %d closest peers to ")
+  struct Peer** result = calloc(n, sizeof(struct Peer*));
+  pointer_not_null(result, "find_closest_peers malloc error");
 
-    log_msg(LOG_DEBUG, "Searching %d closest peers to target %s", n, target_str);
+  char target_str[sizeof(HashID) * 2 + 1] = {0};
+  sha256_to_hex(target, target_str);
 
-    int count = 0;
-    HashID own_id = {0};
+  log_msg(LOG_DEBUG, "Searching %d closest peers to target %s", n, target_str);
 
-    if (get_own_id(&own_id) != 0) {
-        log_msg(LOG_ERROR, "find_closest_peers get_own_id error");
-        return NULL;
-    }
+  int count = 0;
+  HashID own_id = {0};
 
-    HashID dist_to_target = {0};
-    // Calculate distance between us and the target
-    dist_hash(&dist_to_target, &own_id, target);
+  if (get_own_id(&own_id) != 0) {
+    log_msg(LOG_ERROR, "find_closest_peers get_own_id error");
+    return NULL;
+  }
 
-    char dist_str[sizeof(HashID) * 2 + 1] = {0};
-    sha256_to_hex(&dist_to_target, dist_str);
+  HashID dist_to_target = {0};
+  // Calculate distance between us and the target
+  dist_hash(&dist_to_target, &own_id, target);
 
-    log_msg(LOG_DEBUG, "Distance from us to target is %s", dist_str);
+  char dist_str[sizeof(HashID) * 2 + 1] = {0};
+  sha256_to_hex(&dist_to_target, dist_str);
 
-    int bucket_index = get_bucket_index(&dist_to_target);
+  log_msg(LOG_DEBUG, "Distance from us to target is %s", dist_str);
 
-    if (bucket_index == -1) {
-        log_msg(LOG_WARN, "find_closest_peers - Why are we looking for ourselves?");
-        return NULL;
-    }
+  int bucket_index = get_bucket_index(&dist_to_target);
 
-    log_msg(LOG_DEBUG, "Bucket index for this distance is %d", bucket_index);
+  if (bucket_index == -1) {
+    log_msg(LOG_WARN, "find_closest_peers - Why are we looking for ourselves?");
+    return NULL;
+  }
 
-    // First get nodes from the bucket with nodes closest to the target
-    count += find_nearest(&buckets[bucket_index], target, result + count, n - count);
+  log_msg(LOG_DEBUG, "Bucket index for this distance is %d", bucket_index);
 
-    // Extend our range to neighboring buckets until we searched everything or found the number of requested peers
-    int offset = 1;
+  // First get nodes from the bucket with nodes closest to the target
+  count +=
+      find_nearest(&buckets[bucket_index], target, result + count, n - count);
 
-    while (count < n && (bucket_index - offset >= 0 || bucket_index + offset < BUCKET_COUNT)) {
-        if (bucket_index - offset >= 0)
-            count += find_nearest(&buckets[bucket_index - offset], target, result + count, n - count);
-        
-        if (bucket_index + offset < BUCKET_COUNT)
-            count += find_nearest(&buckets[bucket_index + offset], target, result + count, n - count);
-        
-        offset++;
-    }
+  // Extend our range to neighboring buckets until we searched everything or
+  // found the number of requested peers
+  int offset = 1;
 
-    if (count == 0) {
-        free(result);
-        return NULL;
-    }
+  while (count < n &&
+         (bucket_index - offset >= 0 || bucket_index + offset < BUCKET_COUNT)) {
+    if (bucket_index - offset >= 0)
+      count += find_nearest(&buckets[bucket_index - offset], target,
+                            result + count, n - count);
 
-    return result;
+    if (bucket_index + offset < BUCKET_COUNT)
+      count += find_nearest(&buckets[bucket_index + offset], target,
+                            result + count, n - count);
+
+    offset++;
+  }
+
+  if (count == 0) {
+    free(result);
+    return NULL;
+  }
+
+  return result;
 }
 
-void update_bucket_peers(struct Peer* peer) {
-    log_msg(LOG_DEBUG, "update_bucket_peers");
+void update_bucket_peers(Buckets bucket, struct Peer* peer) {
+  if (!peer) {
+    log_msg(LOG_ERROR, "Error in update_bucket_peers peer is NULL!");
+    return;
+  }
+  if (!bucket) {
+    log_msg(LOG_ERROR, "Error in update_bucket_peers bucket is null");
+    return;
+  }
+  HashID own_id;
+  int ret = get_own_id(&own_id);
+
+  if (ret != 0) {
+    log_msg(LOG_ERROR, "Error in update_bucket_peers get_own_id!");
+    return;
+  }
+  HashID distance;
+  dist_hash(&distance, &own_id, &peer->peer_id);
+
+  int bucket_index = get_bucket_index(&distance);
+
+  if (bucket_index == -1) {
+    log_msg(LOG_ERROR, "Error in update_bucket_peers bucket_index!");
+    return;
+  }
+
+  if (bucket[bucket_index].size < BUCKET_SIZE) {
+    log_msg(LOG_DEBUG, "[update_bucket_peers]:Adding new peer to the bucket");
+    add_front(&bucket[bucket_index], peer);
+  } else
+    log_msg(LOG_DEBUG, "[update_bucket_peers]: The Bucket is full");
 }
