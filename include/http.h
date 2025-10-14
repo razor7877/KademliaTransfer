c6c9dd726527b3109f1718a80023ea5b92702932
Hugo Lumet <hugo.lumet@protonmail.com>
2025-10-14 10:52:47 +0200

    Implements http_download_file

--- include/http.h
+++ include/http.h
@@ -1,22 +1,25 @@
 #pragma once
 
-#include <stddef.h>
 #include <poll.h>
+#include <stddef.h>
 
-#include "shared.h"
 #include "bucket.h"
+#include "shared.h"
+#define HTTP_HEADER_SIZE 8192
+#define CHUNK_SIZE 4096
 
 /**
  * @file http.h
  * @brief Implementation of HTTP client server interactions
- * 
- * This file defines functions for handling incoming HTTP request passed on by the network layer, as well as downloading files from other peers
- * 
+ *
+ * This file defines functions for handling incoming HTTP request passed on by
+ * the network layer, as well as downloading files from other peers
+ *
  */
 
 /**
  * @brief Handles a HTTP request
- * 
+ *
  * @param contents The contents of the HTTP request
  * @param length The length of the HTTP packet
  */
@@ -24,7 +27,7 @@ void handle_http_request(struct pollfd* sock, char* contents, size_t length);
 
 /**
  * @brief Downloads a file by hash from the HTTP server served by peer
- * 
+ *
  * @param peer The peer from which to download the file
  * @param file The hash of the file to be downloaded
  * @return int 0 if it was successful, negative number otherwise
